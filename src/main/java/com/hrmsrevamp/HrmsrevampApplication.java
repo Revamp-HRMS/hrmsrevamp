c5f29e0ebc2a1f43c0d926dc41464b9388f8db86
Mustaqim Shaikh <mustaqim@apptware.com>
2024-05-11 23:05:57 +0530

    initial pushed

---
+++ src/main/java/com/hrmsrevamp/HrmsrevampApplication.java
@@ -0,0 +1,13 @@
+package com.hrmsrevamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsrevampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsrevampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrmsrevamp/HrmsrevampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrmsrevamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsrevampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
